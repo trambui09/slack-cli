5d3273055e2a98f16b11f14d09c7e13e30294539
Tram Bui <pbui17@cmc.edu>
2020-10-06 15:18:26 -0500

    created the recipient parent class

---
+++ lib/recipient.rb
@@ -0,0 +1,25 @@
+#Module?
+class Recipient
+  attr_reader :slack_id, :name
+
+  def initialize(slack_id:, name:)
+    @slack_id = slack_id
+    @name = name
+  end
+
+  def send_message(message)
+  end
+
+  def self.get(url, params)
+  end
+
+  def details
+  end
+
+  def self.list_all
+    # implement me in child class
+    raise NotImplementedError.new, "Must implement me in child class!"
+  end
+
+
+end
