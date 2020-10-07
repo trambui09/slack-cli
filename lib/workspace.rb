1f0a377ee3cb4a5c1c4d1b7521dc2e6e77b7ec1f
marks214 <aimee.oz.x@gmail.com>
2020-10-07 12:18:18 -0700

    #select_user method working, need to verify if return value is correct

--- lib/workspace.rb
+++ lib/workspace.rb
@@ -10,20 +10,12 @@ class Workspace
   end
 
   def select_user(id)
-    # if @users.slack_id.nil? || @users.name.nil?
-    #   # raise error
-    # end
     user_array = @users.find do |user|
-       user.slack_id == id
+       user.slack_id == id || user.name == id
     end
 
-    unless user_array
-      raise ArgumentError.new 'user not found'
-    end
-
-
-    selected_user = @users.select { |user| user == id}
-    return selected_user
+    return user_array unless user_array.nil?
+    raise SlackAPIError.new, "user/ID not found"
   end
 
   def select_channel(id)
