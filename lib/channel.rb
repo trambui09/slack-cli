7dabea5e279042c2a1c23e6586569012723e124b
Tram Bui <pbui17@cmc.edu>
2020-10-06 21:31:37 -0500

    worked on the .list_all method

--- lib/channel.rb
+++ lib/channel.rb
@@ -2,9 +2,10 @@
 require_relative 'recipient'
 
 class Channel < Recipient
+  CHANNEL_LIST_URL = 'https://slack.com/api/conversations.list'
   attr_reader :topic, :member_count
 
-  def initialize(topic:, member_count:)
+  def initialize(slack_id:, name:, topic:, member_count:)
     super(slack_id, name)
     @topic = topic
     @member_count = member_count
@@ -14,6 +15,22 @@ class Channel < Recipient
   end
 
   def self.list_all
+
+    response = self.get(CHANNEL_LIST_URL, query: {token: ENV['SLACK_TOKEN']} )
+    if response['ok'] == false
+      raise SlackApiError, "API call failed with code #{response['error']}"
+    end
+
+
+    channels = response['channels'].map do |channel|
+      self.new(slack_id: channel['id'],
+               name: channel['name'],
+               topic: channel['topic'],
+               member_count: channel['num_members']
+      )
+    end
+
+    return channels
   end
 
 
--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,20 @@
 #module Slack
 require_relative 'recipient'
+require 'dotenv'
+require 'httparty'
+
+Dotenv.load
 
 class User < Recipient
+  USER_LIST_URL = 'https://slack.com/api/users.list'
+
+  class SlackApiError < Exception; end
+
   attr_reader :real_name, :status_text, :status_emoji
 
-  def initialize(real_name:, status_text:, status_emoji:)
+  # don't know if this is how we truly initialize this constructor
+
+  def initialize(slack_id:, name:, real_name:, status_text:, status_emoji:)
     super(slack_id, name)
     @real_name = real_name
     @status_text = status_text
@@ -15,6 +25,32 @@ class User < Recipient
   end
 
   def self.list_all
+    # build and send the request
+
+    #token = ENV['SLACK_TOKEN']
+
+    # response = HTTParty.get(USER_LIST_URL, query: {token: ENV['SLACK_TOKEN']})
+
+    response = self.get(USER_LIST_URL, query: {token: ENV['SLACK_TOKEN']} )
+
+    if response['ok'] == false
+      raise SlackApiError, "API call failed with code #{response['error']}"
+    end
+
+
+    users = response['members'].map do |user|
+      self.new(slack_id: user['id'],
+               name: user['name'],
+               real_name: user['real_name'],
+               status_text: user['status_text'],
+               status_emoji: user['status_emoji']
+      )
+    end
+
+    return users
+
+
+
   end
 
 end
